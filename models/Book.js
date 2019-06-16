eee2649716e827b4d0cb54d900de6ea483350b9c
Puspa Sharma <blackice002@gmail.com>
2019-06-15 23:33:26 -0400

    Book Schema created

---
+++ models/Book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: String,
+    authors: [String],
+    Description: String,
+    img: {
+        type: { String }
+    },
+    link: {
+        type: String
+    }
+})
+const Book = mongoose.model("Book", BookSchema);
+module.exports = Book;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express= require('express');
 const app = express();
 const PORT = process.env.PORT || 8080
 
-
 app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 
 // mongoDB database setup
 const mongoose=require("mongoose")
